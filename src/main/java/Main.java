9ca2f59b50e5b346f1e62c888999efbfce2a4cec
sergeybp <sergeybackpath@gmail.com>
2017-08-18 18:56:57 +0300

    Starting working with Twitter API

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,37 @@
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            System.err.println("Args: Username + Password for bbridge API");
-        }
-        BbridgeAPI a = new BbridgeAPI(args[0], args[1]);
 
+        Properties prop = new Properties();
+        InputStream input = new FileInputStream("config.properties");
+        prop.load(input);
+
+        String bbridgeUserName = prop.getProperty("bbridgeusername");
+        String bbridgePassword = prop.getProperty("bbridgepassword");
+        String comsumerKey = prop.getProperty("consumerkey");
+        String consumerSeceret = prop.getProperty("consumersecret");
+        String token = prop.getProperty("accesstoken");
+        String tokenSecret = prop.getProperty("accesssecret");
+
+        BbridgeAPI a = new BbridgeAPI(bbridgeUserName, bbridgePassword);
         ArrayList<String> texts = new ArrayList<>();
-        texts.add("Hi");
-        texts.add("Robots");
-        texts.add("dogs");
-
+        texts.add("CAT");
+        texts.add("DOG");
+        texts.add("TREE");
         ArrayList<String> images = new ArrayList<>();
         images.add("http://www.factroom.ru/facts/wp-content/uploads/2014/02/Koshki-620x465.jpg");
-        //images.add("https://ptzgovorit.ru/sites/default/files/original_nodes/sibirskaya-koshka2.jpg");
-
+        images.add("https://ptzgovorit.ru/sites/default/files/original_nodes/sibirskaya-koshka2.jpg");
         System.out.println(a.fullCycle(texts, images).toString());
+
+        TwitterAPI api = new TwitterAPI(comsumerKey, consumerSeceret, token, tokenSecret);
+        api.tryAccess();
+
+
     }
 
 }
